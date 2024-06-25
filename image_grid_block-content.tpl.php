d31b40f928f20cc96937e5aff9511380c9609e58
Tim Erickson (@stpaultim) <tim@stpaultim.com>
2024-06-25 18:28:52 -0500

    User picks which image to highlight

--- image_grid_block-content.tpl.php
+++ image_grid_block-content.tpl.php
@@ -12,34 +12,114 @@
  *
  * @see template_preprocess_block()
  */
-
-  dpm($variables);
 ?>
 
-
-<div class="image_grid_block_wrapper <?php print $wrapper_classes;?>">
-  <div class="image_grid_block image-one item <?php print $imageclass_1;?>">
-    <?php print $image_1 ?>
-    <div class="image-overlay">
-      <span class="text-container">
-        <?php print $content_1 ?>
-      </span>
+<?php if ($highlighted_image == 'none') { ?>
+  <div class="image_grid_block_wrapper no-tall-image <?php print $wrapper_classes;?>">
+    <div class="image_grid_block image-one item ">
+      <?php print $image_1 ?>
+      <div class="image-overlay">
+        <span class="text-container">
+          <?php print $content_1 ?>
+        </span>
+      </div>
+    </div>
+    <div class="image_grid_block image-two item">
+      <?php print $image_2 ?>
+      <div class="image-overlay">
+        <span class="text-container">
+          <?php print $content_2 ?>
+        </span>
+      </div>
+    </div>
+    <div class="image_grid_block image-three item">
+      <?php print $image_3 ?>
+      <div class="image-overlay">
+        <span class="text-container">
+          <?php print $content_3 ?>
+        </span>
+      </div>
     </div>
   </div>
-  <div class="image_grid_block image-two item <?php print $imageclass_2;?>">
-    <?php print $image_2 ?>
-    <div class="image-overlay">
-      <span class="text-container">
-        <?php print $content_2 ?>
-      </span>
+<?php }elseif ($highlighted_image == 'one') { ?>
+  <div class="image_grid_block_wrapper <?php print $wrapper_classes;?>">
+    <div class="image_grid_block image-one item tall-image">
+      <?php print $image_1 ?>
+      <div class="image-overlay">
+        <span class="text-container">
+          <?php print $content_1 ?>
+        </span>
+      </div>
+    </div>
+    <div class="image_grid_block image-two item">
+      <?php print $image_2 ?>
+      <div class="image-overlay">
+        <span class="text-container">
+          <?php print $content_2 ?>
+        </span>
+      </div>
+    </div>
+    <div class="image_grid_block image-three item">
+      <?php print $image_3 ?>
+      <div class="image-overlay">
+        <span class="text-container">
+          <?php print $content_3 ?>
+        </span>
+      </div>
     </div>
   </div>
-  <div class="image_grid_block image-three item <?php print $imageclass_3;?>">
-    <?php print $image_3 ?>
-    <div class="image-overlay">
-      <span class="text-container">
-        <?php print $content_3 ?>
-      </span>
+<?php } elseif ($highlighted_image == 'two') { ?>
+  <div class="image_grid_block_wrapper <?php print $wrapper_classes;?>">
+    <div class="image_grid_block image-two item tall-image">
+      <?php print $image_2 ?>
+      <div class="image-overlay">
+        <span class="text-container">
+          <?php print $content_2 ?>
+        </span>
+      </div>
+    </div>
+    <div class="image_grid_block image-one item">
+      <?php print $image_1 ?>
+      <div class="image-overlay">
+        <span class="text-container">
+          <?php print $content_1 ?>
+        </span>
+      </div>
+    </div>
+    <div class="image_grid_block image-three item">
+      <?php print $image_3 ?>
+      <div class="image-overlay">
+        <span class="text-container">
+          <?php print $content_3 ?>
+        </span>
+      </div>
     </div>
   </div>
-</div>
+<?php } else { ?>
+  <div class="image_grid_block_wrapper .no-tall-image <?php print $wrapper_classes;?>">
+    <div class="image_grid_block image-one item">
+      <?php print $image_1 ?>
+      <div class="image-overlay">
+        <span class="text-container">
+          <?php print $content_1 ?>
+        </span>
+      </div>
+    </div>
+    <div class="image_grid_block image-three item tall-image">
+      <?php print $image_3 ?>
+      <div class="image-overlay">
+        <span class="text-container">
+          <?php print $content_3 ?>
+        </span>
+      </div>
+    </div>
+    <div class="image_grid_block image-two item">
+      <?php print $image_2 ?>
+      <div class="image-overlay">
+        <span class="text-container">
+          <?php print $content_2 ?>
+        </span>
+      </div>
+    </div>
+  </div>
+<?php } ?>
